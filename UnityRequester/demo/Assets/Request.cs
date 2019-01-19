b1ec2a53cfae2cc720ec7df3e25a6bb8e715ef99
Sifan Ye, on his MBP <ysf199711@126.com>
2019-01-19 16:34:16 -0500

    Unity requester async execution

--- UnityRequester/demo/Assets/Request.cs
+++ UnityRequester/demo/Assets/Request.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 
 using NetMQ;
 using NetMQ.Sockets;
@@ -9,12 +10,68 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
+public class ParallelRequester{
+	private readonly Thread requester;
+
+	private bool requesterStopped;
+
+	private string sendMsg = "";
+
+	public delegate void MessageDelegate(string message);
+
+	public ParallelRequester(){
+		requester = new Thread(RequesterWorker);
+	}
+
+	private void RequesterWorker(){
+		AsyncIO.ForceDotNet.Force();
+
+		var reqSocket = new RequestSocket("tcp://localhost:5556");
+
+		try{
+			while(!requesterStopped){
+				if(!string.Equals(sendMsg, "")){
+					reqSocket.SendFrame(sendMsg);
+					Debug.Log("Send: " + sendMsg);
+					sendMsg = "";
+
+					string msg;
+					if (reqSocket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out msg)) {
+						Debug.Log("Received: " + msg);
+					}
+				}
+			}
+		}finally{
+			if (reqSocket != null) {
+				((IDisposable)reqSocket).Dispose();
+			}
+		}
+	}
+
+	public void Update(string msg){
+		sendMsg = msg;
+	}
+
+	public void Start(){
+		requesterStopped = false;
+		requester.Start();
+	}
+
+	public void Stop(){
+		requesterStopped = true;
+		requester.Join();
+	}
+}
 
 public class Request : MonoBehaviour {
+
+	public ParallelRequester requester;
 
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<InputField>().onEndEdit.AddListener(InputFieldHandler);
+		requester = new ParallelRequester();
+		requester.Start();
 	}
 	
 	// Update is called once per frame
@@ -23,14 +80,6 @@ public class Request : MonoBehaviour {
 	}
 
 	private void InputFieldHandler(string txt){
-		using (var reqSocket = new RequestSocket ("tcp://localhost:5556")) {
-			reqSocket.SendFrame(txt);
-			Debug.Log("Send: " + txt);
-
-			string msg;
-			if (reqSocket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out msg)) {
-				Debug.Log("Receive: " + msg);
-			}
-		}
+		requester.Update(txt);
 	}
 }
