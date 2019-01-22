286ec28a6b7af1fb22e98927eb9092c261bd0be9
Sifan Ye, on his MBP <ysf199711@126.com>
2019-01-21 22:36:59 -0500

    Destroy spawned object after falling

---
+++ UnityRequester/demo/Assets/Destroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.y <= 200) {
+			Destroy();
+		}
+	}
+
+	void Destroy(){
+		Destroy (this.gameObject);
+	}
+}
