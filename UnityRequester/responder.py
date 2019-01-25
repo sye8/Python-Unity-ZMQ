5df316d46a31d3176892af1f515c15fe1cf9abca
Sifan Ye, on his MBP <ysf199711@126.com>
2019-01-25 13:23:16 -0500

    Accomodate byte string

--- UnityRequester/responder.py
+++ UnityRequester/responder.py
@@ -6,15 +6,15 @@ socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5556")
 
 while True:
-    msg = socket.recv()
+    msg = str(socket.recv(), 'utf-8')
     print("Received Request with Message: " + msg)
     args = msg.split()
     if args[0] == "gib":
         if len(args) > 1:
-            socket.send(args[1])
+            socket.send_string(args[1])
         else:
-            socket.send("gib usage: gib [object]")
+            socket.send_string("gib usage: gib [object]")
     elif args[0] == "ping":
-        socket.send("ping back from server")
+        socket.send_string("ping back from server")
     else:
-        socket.send("Unidentified command")
+        socket.send_string("Unidentified command")
